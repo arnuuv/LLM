cbc899fe64b0e31c7285da4b6237473dae7c76cd
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-09-14 14:53:06 +0100

    dropout

--- simple-attention-trainable-weights.py
+++ simple-attention-trainable-weights.py
@@ -99,3 +99,22 @@ print(attn_weights)
 context_length = attn_scores.shape[0]
 mask_simple = torch.tril(torch.ones(context_length, context_length))
 print(mask_simple)
+masked_simple = attn_weights * mask_simple
+print(masked_simple)
+row_sums = masked_simple.sum(dim=-1, keepdim=True)
+masked_simple_norm = masked_simple/row_sums
+print(masked_simple_norm)
+
+mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+masked = attn_scores.masked_fill(mask.bool(), -torch.inf)
+print(masked)
+
+attn_weights = torch.softmax(masked/keys.shape[-1]**0.5, dim=-1)
+print(attn_weights)
+
+
+# dropout
+torch.manual_seed(123)
+dropout = torch.nn.Dropout(0.5)
+example = torch.ones(6, 6)
+print(dropout(example))
