72b77a6e521e843c4e381986812f72cefd7b6160
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-09-14 11:06:00 +0100

    python class for self attention

--- simple-attention-trainable-weights.py
+++ simple-attention-trainable-weights.py
@@ -43,3 +43,25 @@ print(attn_weights_2)
 
 context_vec_2 = attn_weights_2 @ values
 print(context_vec_2)
+
+
+'''
+Python class for self attention
+
+import torch.nn as nn
+class SelfAttention_v1(nn.Module):
+  def __init__(self,d_in,d_out):
+    super().__init__()
+    self.W_query = nn.Parameter(torch.rand(d_in,d_out))
+    self.W_key = nn.Parameter(torch.rand(d_in,d_out))
+    self.W_value = nn.Parameter(torch.rand(d_in,d_out))
+    
+  def forward(self,x):
+    keys = x @ self.W_key
+    values = x @ self.W_value
+    queries = x @ self.W_query
+    attn_score = queries @ keys.T
+    attn_weights = torch.softmax(attn_scores/keys.shape[-1]**0.5, dim=-1)
+    context_vec = attn_weights @ values
+    return context_vec
+'''
