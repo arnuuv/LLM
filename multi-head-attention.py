a6c2a7c8b270367cc072289291640c3f11284267
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-09-15 10:58:30 +0100

    multi head attention

--- multi-head-attention.py
+++ multi-head-attention.py
@@ -59,3 +59,29 @@ mha = MultiHeadAttentionWrapper(
 context_vecs = mha(batch)
 print(context_vecs)
 print(context_vecs.shape)
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert (d_out % num_heads == 0), \
+            "d_out must be divisible by num_heads"
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        )
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
