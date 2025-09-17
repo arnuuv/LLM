998b1b913c9abd166193acffbadaa406fa60168c
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-09-17 14:01:05 +0100

    GPT model architecture

---
+++ GPT-model-architecture.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+import torch
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,
+    "context_length": 1024,
+    "emb_dim": 768,
+    "n_heads": 12,
+    "n_layers": 12,
+    "drop_rate": 0.1,
+    "qkv_bias": False
+}
+
+
+class DummyGPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        self.trf_blocks = nn.Sequential(
+            *[DummyTransformerBlock(cfg)
+              for _ in range(cfg["n_layers"])])
+
+
+class DummyTransformerBlock(nn.Module):
+    pass
