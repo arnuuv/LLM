a16e2aab0367ef0baeee01d46f411cf84f7ca3e5
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-09-21 18:02:11 +0100

    model training

--- transformer.py
+++ transformer.py
@@ -181,3 +181,28 @@ print(out)
 
 total_params = sum(p.numel() for p in model.parameters())
 print("Total number of parameters: ", total_params)
+
+print("Token embedding layer shape: ", model.tok_emb.weight.shape)
+print("Output layer shape: ", model.out_head.weight.shape)
+
+total_params_gpt2 = (total_params - sum(p.numel()
+                     for p in model.out_head.parameters()))
+
+
+# a function to generate text
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+
+
+start_context = "Hello, I am"
+encoded = tokenizer.encode(start_context)
+print("encoded: ", encoded)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+print("Encoded_tensor.shape: ", encoded_tensor.shape)
