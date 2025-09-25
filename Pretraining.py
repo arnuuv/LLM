1a1fdc3f3422929641bad6bae1d865432279f06a
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-09-25 14:57:26 +0100

    TopK function

--- Pretraining.py
+++ Pretraining.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 import tiktoken
 import torch
@@ -193,8 +194,8 @@ for x, y in val_loader:
 
 
 def calc_loss_batch(input_batch, target_batch, model, device):
-    input_batch = input_batch.to_device()
-    target_batch = target_batch.to_device()
+    input_batch = input_batch.to(device)
+    target_batch = target_batch.to(device)
     logits = model(input_batch)
     loss = torch.nn.functional.cross_entropy(
         logits.flatten(0, 1), target_batch.flatten()
@@ -249,7 +250,7 @@ steps:
 8) generate sample text for visual inspection
 '''
 
-
+'''
 def generate_and_print_sample(model, tokenizer, device, start_context):
     model.eval()
     context_size = model.pos_emb.weight.shape[0]
@@ -300,3 +301,93 @@ def train_model_simple(model, train_loader, val_loader, optimizer, device, num_e
             model, tokenizer, device, start_context
         )
     return train_losses, val_losses, track_tokens_seen
+
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+model.to(device)
+optimizer = torch.optim.AdamW(
+    model.parameters(),
+    lr=0.0004, weight_decay=0.1
+)
+num_epochs = 10
+train_losses, val_losses, tokens_seen = train_model_simple(
+    model, train_loader, val_loader, optimizer, device, num_epochs=num_epochs, eval_freq=5, eval_iter=5, start_context="Every effort moves you", tokenizer=tokenizer)
+
+
+# model overfits now
+'''
+
+vocab = {
+    "closer": 0,
+    "every": 1,
+    "effort": 2,
+    "forward": 3,
+    "inches": 4,
+    "moves": 5,
+    "pizza": 6,
+    "toward": 7,
+    "you": 8,
+}
+inverse_vocab = {v: k for k, v in vocab.items()}
+next_token_logits = torch.tensor(
+    [4.51, 0.89, -1.90, 6.75, 1.63, -1.62, -1.89, 6.28, 1.79]
+)
+probas = torch.softmax(next_token_logits, dim=0)
+next_token_id = torch.argmax(probas).item()
+print(inverse_vocab[next_token_id])
+torch.manual_seed(123)
+next_token_id = torch.multinomial(probas, num_samples=1).item()
+print(inverse_vocab[next_token_id])
+
+
+def print_sampled_tokens(probas):
+    torch.manual_seed(123)
+    sample = [torch.multinomial(probas, num_samples=1).item()
+              for i in range(1_000)]
+    sampled_ids = torch.bincount(torch.tensor(sample))
+    for i, freq in enumerate(sampled_ids):
+        print(f"{freq} x {inverse_vocab[i]}")
+
+
+print_sampled_tokens(probas)
+
+
+def softmax_with_temperature(logits, temperature):
+    scaled_logits = logits / temperature
+    return torch.softmax(scaled_logits, dim=0)
+
+
+'''
+temperatures = [1, 0.1, 5]  # A
+scaled_probas = [softmax_with_temperature(
+    next_token_logits, T) for T in temperatures]
+x = torch.arange(len(vocab))
+bar_width = 0.15
+fig, ax = plt.subplots(figsize=(5, 3))
+for i, T in enumerate(temperatures):
+    rects = ax.bar(x + i * bar_width, scaled_probas[i],
+                   bar_width, label=f'Temperature = {T}')
+ax.set_ylabel('Probability')
+ax.set_xticks(x)
+ax.set_xticklabels(vocab.keys(), rotation=90)
+ax.legend()
+plt.tight_layout()
+plt.show()
+'''
+
+top_k = 3
+top_logits, top_pos = torch.topk(next_token_logits, top_k)
+print("Top logits: ", top_logits)
+print("Top positions: ", top_pos)
+
+new_logits = torch.where(
+    condition=next_token_logits < top_logits[-1],
+    input=torch.tensor(float('-inf')),
+    other=next_token_logits
+)
+
+print(new_logits)
+
+topk_probas = torch.softmax(new_logits, dim=0)
+print(topk_probas)
