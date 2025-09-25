84511f2e02e38d9f2bf99ddc51abda1fd7b055ff
arnavanchan42@gmail.com <arnavanchan42@gmail.com>
2025-09-25 15:07:24 +0100

    new training model

--- Pretraining.py
+++ Pretraining.py
@@ -238,7 +238,7 @@ print("Training loss:", train_loss)
 print("Validation loss:", val_loss)
 
 
-''' 
+'''
 steps:
 1) iterate over training epochs
 2) iterate over batches in each training epoch
@@ -391,3 +391,46 @@ print(new_logits)
 
 topk_probas = torch.softmax(new_logits, dim=0)
 print(topk_probas)
+
+
+def generate(model, idx, max_new_tokens, context_size,
+             temperature=1.0, top_k=None, eos_id=None):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        if top_k is not None:
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(
+                logits < min_val,
+                torch.tensor(float('-inf')).to(logits.device),
+                logits)
+        if temperature > 0.0:
+            logits = logits / temperature
+            probs = torch.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+        if idx_next == eos_id:
+            break
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+
+
+torch.manual_seed(123)
+token_ids = generate(
+    model=model,
+    idx=text_to_token_ids("Every effort moves you", tokenizer).to(device),
+    max_new_tokens=15,
+    context_size=GPT_CONFIG_124M["context_length"],
+    top_k=25,
+    temperature=1.4
+)
+print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+torch.save(model.state_dict(), "model.pth")
+model = GPTModel(GPT_CONFIG_124M)
+model.load_state_dict(torch.load("model.pth", map_location=device))
+model.eval()
